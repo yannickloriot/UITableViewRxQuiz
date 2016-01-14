a972fbf8197c94838b75307ca241783a90f640c9
Yannick Loriot <yannick.loriot@gmail.com>
2016-01-14 17:59:47 +0100

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+pod 'RxSwift', '~> 2'
+pod 'RxCocoa', '~> 2'
---
+++ UITableViewRxQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewRxQuiz
+//
+//  Created by Yannick LORIOT on 14/01/16.
+//  Copyright © 2016 Yannick LORIOT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
